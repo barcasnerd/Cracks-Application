c1ff5ed939a37f55c589033ec6415c1569ba4e5d
Jair Barcasnegras <jebarcasnegras@uninorte.edu.co>
2019-05-08 21:36:42 -0500

    Diseño

---
+++ src/classes/Cracks.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import Frames.Principal;
+
+/**
+ *
+ * @author Jair
+ */
+public class Cracks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+    }
+    
+}
